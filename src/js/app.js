9b01092ae30eba72e8f01a1b84c069253f0b9c04
Vladislav Ometov <ometov96@mail.ru>
2022-11-26 15:48:50 +0700

    Сделаны input -/+

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,29 @@ flsFunctions.isWebp();
 
 import $ from "jquery";
 
-$("button").click(function(){
-    $("#test").toggle();
-});
+//Input +/-
+$(".count_input input").keyup(function () {
+    var value = $(this).val().replace(/[^\d]/g, '');
+    var inputInsert = $(this).data("insert");
+
+    if (value < 1){
+        var value = 1;
+        $(this).val(value + " " + inputInsert);
+    }
+    $(this).val(value + " " + inputInsert);
+}).keyup()
+
+$(".count_input .arithmetic").click(function (){
+    var inputInsert = $(this).parent(".count_input").find("input").data("insert");
+
+    var arithmetic = Number($(this).data("arithmetic"));
+    var value = Number($(this).parent(".count_input").find("input").val().replace(/[^\d]/g, ''));
+
+    if (value + arithmetic < 1){
+        value = 1;
+    }else{
+        value = value + arithmetic;
+    }
+
+    $(this).parent(".count_input").find("input").val(value + " " + inputInsert);
+})
